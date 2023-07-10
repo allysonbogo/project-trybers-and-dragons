1216723c65aad6bbc88457f107d2b97fd5e8a582
Allyson Bogo <ab.bogo@gmail.com>
2023-07-10 14:22:35 -0300

    implements abstract class Race

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 function getRandomInt(min: number, max: number) {
   const newMin = Math.ceil(min);
-  // const newMax = Math.floor(max);
+  const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
 }
 
